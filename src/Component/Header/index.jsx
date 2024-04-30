1df253a709ae6c901aa28302cc623a043b7da3f9
Ahmed-NEEDGlobal <ahmed.needglobal@gmail.com>
2024-04-30 17:59:05 +0600

    layout

--- src/Component/Header/index.jsx
+++ src/Component/Header/index.jsx
@@ -3,10 +3,19 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex space-x-4">
-      <Link href={"/"}>Home</Link>
-      <Link href={"/about_us"}>About Us</Link>
-      <Link href={"/contact_us"}>Contact Us</Link>
+    <div className="flex flex-row gap-4">
+      <div className="text-red-900">
+        {/* <Link className="bg-gray-600 text text-yellow-500" href={"/"}>
+          Home
+        </Link> */}
+        home xda
+      </div>
+      <div className="bg-yellow-400">
+        <Link href={"/about_us"}>About Us</Link>
+      </div>
+      <div className="bg-cyan-700">
+        <Link href={"/contact_us"}>Contact Us</Link>
+      </div>
     </div>
   );
 };
